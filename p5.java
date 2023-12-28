c812ee7effee4815cb785b1d50a8b8bbde3abf86
Soumadeep Sarkar <72089988+Soumadeep360@users.noreply.github.com>
2023-12-28 23:16:34 +0530

    Update p5.java

--- p5.java
+++ p5.java
@@ -1,27 +1,18 @@
 import java.util.*;
 class p5{
 
-
     static void strcmp(String s1,String s2){
         if(s1.compareTo(s2)==0)
         System.out.println("Both strings are equal");
         else
         System.out.println("Both strings are not equal");
 
-
     }
     static void strcmp(String s1,String s2,int n){
-        int i=0;
-        for(i=0;i<n;i++){
-            if(s1.charAt(i)!=s2.charAt(i))break;
-        }
-        if(i==n)System.out.println("Substring of length n is equal");
+        if(s1.substring(0,n).equals(s2.substring(0,n)))
+        System.out.println("Equal upto length "+n);
         else
-        System.out.println("nOT equal");
-
-
-
-
+        System.out.println("not equal!!");
     }
     public static void main(String args[]){
      Scanner sc=new Scanner(System.in);
@@ -30,10 +21,9 @@ class p5{
         System.out.println("Enter 2 strings");
         s1=sc.next();
         s2=sc.next();
-        System.out.println("Enter the number:");
+        System.out.println("Enter the number upto which charecters to be checked:");
         n=sc.nextInt();
-
-
+        
         strcmp(s1,s2);
         strcmp(s1,s2,n);        
     }
